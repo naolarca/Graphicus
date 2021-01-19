d8861ea21e6c04de2bae8405bc969bcc0cc94241
naolarca <nao15@live.ca>
2021-01-19 18:30:41 -0500

    ajout plan de test

--- Graphicus-01/tests.cpp
+++ Graphicus-01/tests.cpp
@@ -426,113 +426,70 @@ void Tests::tests_application_cas_01()
 void Tests::tests_application_cas_02()
 {
    cout << "TESTS APPLICATION (CAS 02)" << endl;  
+   cout << "PLAN DE TEST" << endl; 
+   cout << "" << endl;  
    
    Canevas CanValid;
    Coordonnee c;
    Dimension d;
    
-   cout<< "***Étape 1 : Activer la couche 1"<<endl;
-   CanValid.activerCouche(1);
-   
-   cout<< "***Étape 2 : Ajouter trois formes géométriques"<<endl;
-   cout<< "Les forment ajouté à la couche sont les suivante :"<< endl;
-   c.x = -4;
-   c.y = 0;
-   cout << "Cercle(x = "<< c.x << ", y = " << c.y << ", rayon = 6)"<< endl;
-   Forme* cc1 = new Cercle(c, 6);
-   CanValid.ajouterForme(cc1);
-   
-   c.x = 1;
-   c.y = 2;
-   d.l = 3;
-   d.h = 4;
-   cout << "Rectangle(x = "<< c.x << ", y = " << c.y << ", l = "<< d.l << ", h = " << d.h << ")" << endl;
-   Forme* rc1 = new Rectangle(c, d);
-   CanValid.ajouterForme(rc1);
-   
-   c.x = 6;
-   c.y = 6;
-   cout << "Carre(x = " << c.x << ", y = " << c.y << ", cote = 6)"<< endl;
-   Forme* cac1 = new Carre(c, 6);
-   CanValid.ajouterForme(cac1);
-   
-   cout << "***Étape 3 : Activer la couche 2"<< endl;
-   CanValid.activerCouche(2);
-   
-   cout << "***Étape 4 : Ajouter une forme"<< endl;
-   c.x = 0;
-   c.y = 5;
-   
-   cout<< "La forme ajouté à la couche est la suivante :"<< endl;
-   cout << "Cercle(x = "<< c.x << ", y = " << c.y << ", rayon = 1)"<< endl;
-   Forme* cc2 = new Cercle(c,1);
-   CanValid.ajouterForme(cc2);
-   
-   cout << "***Étape 5 : Afficher le canevas"<< endl;
-   CanValid.afficher(std::cout);
-   
-   cout<< "***Étape 6 : Afficher l'aire du canevas" << endl;
-   cout << "L'aire total du caneva est de :" << CanValid.aire() << endl;
-   
-   cout<< "***Étape 7 : Activer la couche 0"<< endl;
+   cout<< "*** TEST 1"<<endl;
+   cout<< "Nom de la fonction à tester: Retirer forme a partir du canevas"<<endl;
+   cout<< "Entrée: Un index plus grand que la taille"<<endl;
    CanValid.activerCouche(0);
+   if (CanValid.retirerForme(3)==true) cout<<"Forme retirée"<<endl;
+   else cout<<"erreur"<<endl;
    
-   cout << "***Étape 8 : Ajouter trois formes géométrique différentes" << endl;
-   cout<< "Les forment ajouté à la couche sont les suivante :"<< endl;
+   cout<< "*** TEST 2"<<endl;
+   cout<< "Nom de la fonction à tester: Retirer forme a partir du canevas"<<endl;
+   cout<< "Entrée: Un index négatif"<<endl;
+   if (CanValid.retirerForme(-2)==true) cout<<"forme retirée"<<endl;
+   else cout<<"erreur"<<endl;
    
-   c.x = 7;
-   c.y = -7;
-   cout << "Cercle(x = "<< c.x << ", y = " << c.y << ", rayon = 3)"<< endl;
-   Forme* cc0 = new Cercle(c, 3);
-   CanValid.ajouterForme(cc0);
+   cout<< "*** TEST 3"<<endl;
+   cout<< "Nom de la fonction à tester: Retirer forme a partir du canevas"<<endl;
+   cout<< "Entrée: Un index plus grand que la capacité"<<endl;
+   if (CanValid.retirerForme(14)==true) cout<<"Forme retirée"<<endl;
+   else cout<<"erreur"<<endl;
    
-   c.x = 3;
-   c.y = 2;
-   d.l = 6;
-   d.h = 1;
-   cout << "Rectangle(x = "<< c.x << ", y = " << c.y << ", l = "<< d.l << ", h = " << d.h << ")" << endl;
-   Forme* rc0 = new Rectangle(c, d);
-   CanValid.ajouterForme(rc0);
+   cout<< "*** TEST 4"<<endl;
+   cout<< "Nom de la fonction à tester: Réinitialiser le canevas"<<endl;
+   cout<< "Entrée: Un canevas vide"<<endl;
+   if (CanValid.reinitialiser()==true) cout<<"canevas reinitialisé"<<endl;
+   else cout<<"erreur"<<endl;
    
-   c.x = 2;
-   c.y = 6;
-   cout <<"Carre(x = "<< c.x << ", y = " << c.y << ", cote = 7)"<< endl;
-   Forme* cac0 = new Carre(c, 7);
-   CanValid.ajouterForme(cac0);
+   cout<< "*** TEST 5"<<endl;
+   cout<< "Nom de la fonction à tester: activer une couche a partir du canevas"<<endl;
+   cout<< "Entrée: Un index plus grand que le nombre de couches possibles"<<endl;
+   if (CanValid.activerCouche(7)==true) cout<<"couche activée"<<endl;
+   else cout<<"erreur"<<endl;
    
-   cout << "***Étape 9 : Cacher la couche 2" <<endl;
-   CanValid.cacherCouche(2);
+   cout<< "*** TEST 6"<<endl;
+   cout<< "Nom de la fonction à tester: activer une couche a partir du canevas"<<endl;
+   cout<< "Entrée: Un index négatif"<<endl;
+   if (CanValid.activerCouche(-3)==true) cout<<"couche activée"<<endl;
+   else cout<<"erreur"<<endl;
    
-   cout << "***Étape 10 : Activer la couche 1"<< endl;
-   CanValid.activerCouche(1);
+   cout<< "*** TEST 7"<<endl;
+   cout<< "Nom de la fonction à tester: calculer l'aire d'un cercle"<<endl;
+   cout<< "Entrée: Un rayon négatif"<<endl;
+   c.x=0;
+   c.y=0;
+   Cercle testCercle (c, -5);
+   cout<<"L'aire du cercle est:"<<testCercle.aire()<<endl;
    
-   cout << "***Étape 11 : Translater la couche avec des valeurs non nulles"<<endl;
-   cout << "Les valeurs utilisé sont : (x = 10, y= 10)" << endl;
-   CanValid.translater(10,10);
+   cout<< "*** TEST 8"<<endl;
+   cout<< "Nom de la fonction à tester: calculer l'aire d'un rectangle"<<endl;
+   cout<< "Entrée: Une hauteur négative et une largeur positive"<<endl;
+   d.h=-3;
+   d.l=3;
+   Rectangle testRect (c, d);
+   cout<<"L'aire du rectangle est :"<<testRect.aire()<<endl;
    
-   cout << "***Étape 12 : Afficher le canevas" << endl;
-   CanValid.afficher(std::cout);
-   
-   cout << "***Étape 13 : Afficher l'aire du canevas"<<endl;
-   cout << "L'aire total du caneva est de :" << CanValid.aire() << endl;
-   
-   cout << "***Étape 14 : Retirer la première forme de la couche" << endl;
-   CanValid.retirerForme(0);
-   
-   cout << "***Étape 15 : Afficher le canevas"<< endl; 
-   CanValid.afficher(std::cout);
-   
-   cout << "***Étape 16 : Afficher l'aire du canevas" <<endl;
-   cout << "L'aire total du caneva est de :" << CanValid.aire() << endl;
-   
-   cout << "***Étape 17 : Réinitialiser le canevas"<< endl;
-   CanValid.reinitialiser();
-   
-   cout << "***Étape 18 : Afficher le canevas"<< endl; 
-   CanValid.afficher(std::cout);
-   
-   cout << "***Étape 19 : Afficher l'aire du canevas" <<endl;
-   cout << "L'aire total du caneva est de :" << CanValid.aire() << endl;
-   
-   cout << "Fin du test de validation"<< endl;
+   cout<< "*** TEST 8"<<endl;
+   cout<< "Nom de la fonction à tester: Changer l'état d'une couche"<<endl;
+   cout<< "Entrée: Un état qui n'existe pas comme bonjour"<<endl;
+   Couche NomCouche;
+   if (NomCouche.ChangementEtat("Bonjour")==true) cout<<"Changement d'état réussi"<<endl;
+   else cout<<"erreur"<<endl;
 };
