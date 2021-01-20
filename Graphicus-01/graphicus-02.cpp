1615b27d963f5b78cc59f4bd8b4ebba53f7564d4
beal5070 <beal5070@usherbrooke.ca>
2021-01-19 19:28:35 -0500

    Vrai final

--- Graphicus-01/graphicus-02.cpp
+++ Graphicus-01/graphicus-02.cpp
@@ -4,6 +4,8 @@
  * Date: 08 janvier 2018 (creation)
  * Description: gestionnaire de tests pour l'application Graphicus. Ce
  *    fichier fait partie de la distribution de Graphicus.
+ * Projet réalisé par : Louis-Charles Beaupré - beal5070
+ 			 Naomie L'archevêque Carrière - larn5378
 ********/
 #include "tests.h"
 
